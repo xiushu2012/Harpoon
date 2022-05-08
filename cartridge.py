ab0de091bf1ad611ce8edb5c2454df3b67080a51
weaver <weavernet@163.com>
2022-05-08 22:23:21 +0800

    1.add the abnormal flag in result 2.add the max and min predict data

--- cartridge.py
+++ cartridge.py
@@ -20,6 +20,7 @@ import datetime
 import os
 import matplotlib.pyplot as plt
 import openpyxl
+from sklearn.cluster import DBSCAN
 
 def get_akshare_daily(xlsfile,stock):
 	shname='daily'
@@ -61,12 +62,82 @@ def getkellybEx(value,v0,v100):
 	return kellyb
 
 
+def cal_close_inc(volume_df,close):
+	for i,r in volume_df.iterrows():
+			if r[datecolumn] == date:
+				return r[mvcolumn]
+	return 0
+
 def get_dailyinc_df(path,name,ratio):
 	bond_cov_dailyinc_df = pd.read_excel(path, name)[['date', 'open','high']]
 	bond_cov_dailyinc_df[ratio] = bond_cov_dailyinc_df.apply(lambda row: (row['high']-row['open'])/row['open'], axis=1)
 	bond_cov_biginc_df = bond_cov_dailyinc_df[ bond_cov_dailyinc_df[ratio] > 0.03]
 	return bond_cov_biginc_df
 
+def get_abnormal_dbscan_df(path,name):
+	bond_cov_volume_df = pd.read_excel(path, name)[['date','open','high','low','close','volume']]
+	volumesta = bond_cov_volume_df['volume'].describe(percentiles = [0.3,0.7])
+	valuelow =  volumesta['30%']
+	valuehigh = volumesta['70%']
+	middle_volume_df = bond_cov_volume_df[ (bond_cov_volume_df[['volume']] < valuehigh) & (bond_cov_volume_df[['volume']] > valuelow)]
+	
+	middlesta = middle_volume_df['volume'].describe()
+	middlepes = middlesta['std']
+	print("valuelow:%f,valuehigh:%f,EPS:%f" %(valuelow,valuehigh,middlepes))
+	
+
+	outlier_det=DBSCAN(min_samples=2,eps=middlepes)
+	clusters = outlier_det.fit_predict(bond_cov_volume_df[['volume']].values)
+	print("abnormal volumn:%d" % list(clusters).count(-1))
+	bond_cov_volume_df['flag'] = clusters
+
+	
+	bond_cov_abnormal_df = bond_cov_volume_df[ bond_cov_volume_df['flag'] == -1]
+	
+	
+	abnormal_index = bond_cov_abnormal_df.index.values.tolist()
+	for cur, nxt in zip (abnormal_index, abnormal_index [1:] ):
+		print (cur, nxt)
+
+		#左右都闭区间
+		abnormal_next_df = bond_cov_volume_df.loc[cur:nxt,:][['open','high','low','close']]
+		#cur当天数据不能纳入计算
+		abnormal_next_array = abnormal_next_df.values[1::,:]
+		print(abnormal_next_array)
+		abnormal_cur_close =  abnormal_next_df.loc[cur,'close']
+
+		bond_cov_abnormal_df.loc[cur,'max'] = 100*(np.max(abnormal_next_array)-abnormal_cur_close)/abnormal_cur_close
+		bond_cov_abnormal_df.loc[cur,'min'] = 100*(np.min(abnormal_next_array)-abnormal_cur_close)/abnormal_cur_close
+
+	#print(bond_cov_volume_df)
+	if not os.path.exists(path):
+		bond_cov_abnormal_df.to_excel(writer, 'abnormal')
+	else:
+		with pd.ExcelWriter(path,engine='openpyxl',mode='a',if_sheet_exists='replace') as writer:
+			bond_cov_abnormal_df.to_excel(writer, 'abnormal')	
+	return bond_cov_abnormal_df
+
+
+def get_abnormal_standard_df(path,name):
+	bond_cov_volume_df = pd.read_excel(path, name)[['date','volume']]
+
+	volume_std = np.std(bond_cov_volume_df[['volume']])
+	volume_mean = np.mean(bond_cov_volume_df[['volume']])
+	volume_cutoff = volume_std*1
+	volume_low = volume_mean - volume_cutoff
+	volume_high = volume_mean + volume_cutoff
+	print(volume_low,volume_high)
+
+
+	bond_cov_volume_df = bond_cov_volume_df[ (bond_cov_volume_df[['volume']] < volume_low) | (bond_cov_volume_df[['volume']] > volume_high)]
+	print(bond_cov_volume_df)
+	
+	if not os.path.exists(path):
+		bond_cov_volume_df.to_excel(writer, 'abnormal',index=False)
+	else:
+		with pd.ExcelWriter(path,engine='openpyxl',mode='a') as writer:
+			bond_cov_volume_df.to_excel(writer, 'abnormal',index=False)	
+	return bond_cov_volume_df
 
 
 def get_daily_df(path,name,price):
@@ -105,8 +176,8 @@ if __name__=='__main__':
 
 		bond_interest_df = pd.read_excel(interestpath, 'clause')
 
-		#bond_kelly_df = pd.DataFrame(columns=['名称','代码', '胜率', '赔率', '下注比例', '当前价格', '00分位', '25分位', '50分位', '75分位','100分位','75涨幅','回落参考','绝对参考'])
-		bond_kelly_df = pd.DataFrame(columns=['名称', '代码', '胜率', '赔率', '下注比例', '当前价格', '00分位', '25分位', '50分位', '75分位', '100分位', '75涨幅'])
+
+		bond_kelly_df = pd.DataFrame(columns=['名称', '代码', '胜率', '赔率', '下注比例', '当前价格', '00分位', '25分位', '50分位', '75分位', '100分位', '75涨幅','年均异动','最后异动'])
 		money = 'money'
 		ratio = 'ratio'
 		for i, bondrow in bond_interest_df.iterrows():
@@ -124,7 +195,7 @@ if __name__=='__main__':
 
 			bond_cov_biginc_df = get_dailyinc_df(resultpath, insheetname,ratio)
 			incsta = bond_cov_biginc_df[ratio].describe()
-
+			
 			print(dailysta)
 			print(incsta)
 
@@ -150,15 +221,16 @@ if __name__=='__main__':
 			#下注比例
 			kellyf1 = ((kellyb1+1)*kellyp-1)/kellyb1
 
+			#异动指标
+			bond_cov_abnormal_df = get_abnormal_dbscan_df(resultpath,insheetname)
+			abnormalcount = len(bond_cov_abnormal_df)
+			print("abnormalcount:%d" % abnormalcount)
+			#250个交易日
+			abnormalperyear = abnormalcount/(counts/250)
+			abnormallatest = bond_cov_abnormal_df.iloc[-1][0]
+			
 
-			#回落卖出参考=当前价格*（1+75涨幅）
-			#returnsell = value *(1+inc75)
-			#绝对卖出参考=75分位价格
-			#absolutesell = value75
-
-			#bond_kelly_df = bond_kelly_df.append({'名称':name,'代码':bond,'胜率':kellyp,'赔率':kellyb1,'下注比例':kellyf1,'当前价格':value,'00分位':valuemin,'25分位':value25,'50分位':value50,'75分位':value75,'100分位':valuemax,'75涨幅':inc75,'回落参考':returnsell,'绝对参考':absolutesell},ignore_index=True)
-
-			bond_kelly_df = bond_kelly_df.append({'名称':name,'代码':bond,'胜率':kellyp,'赔率':kellyb1,'下注比例':kellyf1,'当前价格':value,'00分位':valuemin,'25分位':value25,'50分位':value50,'75分位':value75,'100分位':valuemax,'75涨幅':inc75},ignore_index=True)
+			bond_kelly_df = bond_kelly_df.append({'名称':name,'代码':bond,'胜率':kellyp,'赔率':kellyb1,'下注比例':kellyf1,'当前价格':value,'00分位':valuemin,'25分位':value25,'50分位':value50,'75分位':value75,'100分位':valuemax,'75涨幅':inc75,'年均异动':abnormalperyear,'最后异动':abnormallatest},ignore_index=True)
 
 			print("名称,胜率，赔率,下注比例:",name,kellyp,kellyb1,kellyf1)
 		#print(bond_kelly_df)
