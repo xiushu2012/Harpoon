dd958d3995c0162bacfdb0ed122b9756cbc7a1b6
unknown <xiushu2012@github.com>
2022-10-05 22:33:57 +0800

    1.dream value base on max(baseval,75value and 75percent value)

--- cartridge.py
+++ cartridge.py
@@ -221,7 +221,7 @@ if __name__=='__main__':
 		bond_interest_df = pd.read_excel(interestpath, 'clause')
 
 
-		bond_kelly_df = pd.DataFrame(columns=['名称', '代码', '胜率', '赔率', '下注比例', '当前价格', '期望价格','00分位', '25分位', '50分位', '75分位', '100分位', '75涨幅','剩余规模','交易周期','年均异动','最后异动','异动阈值'])
+		bond_kelly_df = pd.DataFrame(columns=['名称', '代码', '胜率', '赔率', '下注比例', '当前价格','期望涨幅','期望价格','保底价格','75涨幅价','75分位价','00分位', '50分位', '100分位' ,'剩余规模','交易周期','年均异动','最后异动','异动阈值'])
 		money = 'money'
 		ratio = 'ratio'
 		for i, bondrow in bond_interest_df.iterrows():
@@ -261,6 +261,7 @@ if __name__=='__main__':
 			value75 = dailysta['75%']
 
 			inc75 = incsta['75%']
+			valinc75=(inc75+1)*value
 			# 赔率=获胜时的盈利/失败时的亏损
 			kellyb1= getkellybEx(value,valuemin,valuemax)
 
@@ -285,9 +286,11 @@ if __name__=='__main__':
 			except Exception as result:
 				print("get abnormal error:" + bond)
 				continue
-
+			
+			dreamval = max(expval,valinc75,value75)
+			dreampercent = 100*(dreamval-value)/value
 			bond_kelly_df = bond_kelly_df.append({'名称':name,'代码':bond,'胜率':kellyp,'赔率':kellyb1,'下注比例':kellyf1,
-			'当前价格':value,'期望价格':expval,'00分位':valuemin,'25分位':value25,'50分位':value50,'75分位':value75,'100分位':valuemax,'75涨幅':inc75,
+			'当前价格':value,'期望涨幅':dreampercent,'期望价格':dreamval,'保底价格':expval,'75涨幅价':valinc75,'75分位价':value75,'00分位':valuemin,'50分位':value50,'100分位':valuemax,
 			'剩余规模':remain,'交易周期':tradeyears,'年均异动':abnormalperyear,'最后异动':abnormallatest,'异动阈值':abnormalminvol},ignore_index=True)
 
 			print("名称,胜率，赔率,下注比例:",name,kellyp,kellyb1,kellyf1)
